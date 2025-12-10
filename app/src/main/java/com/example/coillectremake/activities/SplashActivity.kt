40a3d0e891ab962101ecad61e5b2f526dc781ef3
redlintles <treelands54@gmail.com>
2025-12-09 21:08:14 -0300

    Criada tela splash, alterada Launcher Activity no AndroidManifest

--- app/src/main/java/com/example/coillectremake/activities/SplashActivity.kt
+++ app/src/main/java/com/example/coillectremake/activities/SplashActivity.kt
@@ -1,14 +1,34 @@
 package com.example.coillectremake.activities
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.coillectremake.R
+import androidx.compose.ui.platform.ComposeView
+
+import com.example.coillectremake.layouts.SplashActivityLayout
 
 class SplashActivity : AppCompatActivity() {
+    private val SPLASH_DELAY: Long = 2000;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        setContentView(
+            ComposeView(this).apply {
+                setContent {
+                    SplashActivityLayout()
+                }
+            }
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_DELAY)
+
     }
 }
--- app/src/main/java/com/example/coillectremake/layouts/SplashActivityLayout.kt
+++ app/src/main/java/com/example/coillectremake/layouts/SplashActivityLayout.kt
@@ -1,2 +1,38 @@
 package com.example.coillectremake.layouts
 
+import com.example.coillectremake.R
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun SplashActivityLayout(): Unit {
+    Box(
+        modifier = Modifier.fillMaxSize().background(
+            brush = Brush.verticalGradient(
+                colors = listOf(
+                    Color(0xFF00BCD4),
+                    Color(0XFF4CAF50)
+                )
+            )
+        )
+    ) {
+        Image(
+            painter = painterResource(R.drawable.logo),
+            contentDescription = "Logo cOillect",
+            modifier = Modifier.size(220.dp).align(Alignment.TopCenter).padding(top = 252.dp)
+        )
+    }
+}
