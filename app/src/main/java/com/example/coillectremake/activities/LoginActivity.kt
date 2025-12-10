1350915fdbcba823bd393376cec0035849fde767
redlintles <treelands54@gmail.com>
2025-12-10 00:39:51 -0300

    Adicionada LoginActivity

--- app/src/main/java/com/example/coillectremake/activities/LoginActivity.kt
+++ app/src/main/java/com/example/coillectremake/activities/LoginActivity.kt
@@ -3,12 +3,22 @@ package com.example.coillectremake.activities
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.platform.ComposeView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.coillectremake.R
+import com.example.coillectremake.layouts.LoginActivityLayout
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(
+            ComposeView(this).apply {
+                setContent {
+                    LoginActivityLayout()
+                }
+            }
+        )
     }
 }
--- app/src/main/java/com/example/coillectremake/layouts/LoginActivityLayout.kt
+++ app/src/main/java/com/example/coillectremake/layouts/LoginActivityLayout.kt
@@ -1,2 +1,10 @@
 package com.example.coillectremake.layouts
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun LoginActivityLayout(): Unit {
+
+}
