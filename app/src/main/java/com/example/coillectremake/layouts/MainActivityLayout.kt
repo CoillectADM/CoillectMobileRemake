3fc82efcd7c82cce7544c0cf16644ec78905a5fa
redlintles <treelands54@gmail.com>
2025-12-09 21:20:54 -0300

    Adicionado @Preview aos layouts, finalizada splash screen

--- app/src/main/java/com/example/coillectremake/layouts/MainActivityLayout.kt
+++ app/src/main/java/com/example/coillectremake/layouts/MainActivityLayout.kt
@@ -1,11 +1,13 @@
 package com.example.coillectremake.layouts
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 import com.example.coillectremake.components.SimpleComposeScreen
 
 
 @Composable
+@Preview
 fun MainActivityLayout(): Unit {
     SimpleComposeScreen()
 }
--- app/src/main/java/com/example/coillectremake/layouts/SplashActivityLayout.kt
+++ app/src/main/java/com/example/coillectremake/layouts/SplashActivityLayout.kt
@@ -3,36 +3,34 @@ package com.example.coillectremake.layouts
 import com.example.coillectremake.R
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 
 @Composable
+@Preview
 fun SplashActivityLayout(): Unit {
     Box(
-        modifier = Modifier.fillMaxSize().background(
-            brush = Brush.verticalGradient(
-                colors = listOf(
-                    Color(0xFF00BCD4),
-                    Color(0XFF4CAF50)
-                )
-            )
+        modifier = Modifier.fillMaxSize().paint(
+            painterResource(id = R.drawable.bgdegrade),
+            contentScale = ContentScale.Crop,
+            alignment = Alignment.Center
+
         )
     ) {
         Image(
             painter = painterResource(R.drawable.logo),
             contentDescription = "Logo cOillect",
-            modifier = Modifier.size(220.dp).align(Alignment.TopCenter).padding(top = 252.dp)
+            modifier = Modifier.size(220.dp,220.dp).align(Alignment.Center),
         )
     }
 }
